570f31cd22e191274d528d522abe929d145fcb41
suomi <kopera.marcel422@gmail.com>
2025-04-08 15:00:29 +0900

    updated README

--- README.md
+++ README.md
@@ -3,4 +3,23 @@
 To run do the steps bellow:
 1. cd veggie-tomo
 2. sudo chmod +x run.sh
-3. ./run.sh 
+3. ./run.sh
+
+
+
+API calls
+
+```
+<!-- Get JWT Token -->
+curl -X POST http://localhost:3000/signin \
+    -H "Content-Type: application/json" \
+    -d '{"email": "test@test.test", "password": "1234567890"}'
+
+
+
+<!-- Register vegetable type -->
+curl -X POST http://localhost:3000/register_vegetable \
+    -H "Authorization: Bearer <JWT TOKEN>" \
+    -H "Content-Type: application/json" \
+    -d '{"name": "はす"}'
+```
--- src/routes/api_handlers/api_router.rs
+++ src/routes/api_handlers/api_router.rs
@@ -17,12 +17,3 @@ pub fn api_routes(app_state: Arc<AppState>) -> Router<Arc<AppState>> {
         .route("/api/register_vegetable", post(register_vegetables))
         .layer(middleware::from_fn_with_state(app_state.clone() ,jwt::authorize))
 }
-
-
-
-
-
-// curl -X POST http://localhost:3000/register_vegetable \
-// -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE3NDM5OTM1MTIsImVtYWlsIjoidGVzdEB0ZXN0LnRlc3QifQ.mJ86qwlrYd5Xdky41Z1odQj4Uv3XtC-tKvLJybVrm7Y" \
-// -H "Content-Type: application/json" \
-// -d '{"name": "はす"}'
